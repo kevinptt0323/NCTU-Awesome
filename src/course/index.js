bb162a9e9cad0ccab182da840a99b4970b93a7c5
JubeatWwW <l6104400@gmail.com>
2018-01-13 23:21:04 +0800

    constructure refactor

--- src/course/index.js
+++ src/course/index.js
@@ -63,7 +63,33 @@ switch(currentPage()) {
     skip();
     break;
   case "MAIN":
-    window.location = 'inMenu.asp';
+    $('frameset').remove();
+    document.body = document.createElement('body');
+    const $content = $.parseHTML(`
+ <div class="mdl-layout mdl-js-layout mdl-layout--fixed-drawer mdl-layout--fixed-header">
+  <header class="mdl-layout__header">
+    <div class="mdl-layout__header-row">
+      <span class="mdl-layout-title">Title</span>
+      <div class="mdl-layout-spacer"></div>
+      <nav class="mdl-navigation mdl-layout--large-screen-only">
+        <a class="mdl-navigation__link" href="">Link</a>
+        <a class="mdl-navigation__link" href="">Link</a>
+        <a class="mdl-navigation__link" href="">Link</a>
+        <a class="mdl-navigation__link" href="">Link</a>
+      </nav>
+    </div>
+  </header>
+  <div class="mdl-layout__drawer">
+    <iframe src="inTitle.asp"></iframe>
+    <iframe src="inMenu.asp"></iframe>
+  </div>
+  <main class="mdl-layout__content">
+    <iframe name="frmMain" src="inHelp.asp"></iframe>
+  </main>
+</div>
+    `);
+
+    $('body').append($content);
     break;
 }
 
